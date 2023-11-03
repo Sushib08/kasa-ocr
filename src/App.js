437a2ded8620d9ecf1bac60cd271502a6db0d83b
Maéva Lagrost <johndoe@exemple.com>
2023-11-03 18:00:17 +0100

    init react-router + add data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
 function App() {
-  return <h1>KASA</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
