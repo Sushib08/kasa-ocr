72c0be5c283d666d0e0c27f4a8b9765f87f4c803
Maéva Lagrost <johndoe@exemple.com>
2023-11-11 09:31:48 +0100

    redirect error404

--- src/Pages/Lodgment.jsx
+++ src/Pages/Lodgment.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import ListingData from "../data/listing.json";
+import Error404 from "./Error404"; // Make sure to import your Error404 component
 
 const Lodgment = () => {
   const { id } = useParams();
   const lodgingDetails = ListingData.find((lodging) => lodging.id === id);
+
+  if (!lodgingDetails) {
+    return <Navigate to="/404" />;
+  }
 
   return (
     <div>
